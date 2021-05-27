0e90dbe0fa3146b9be340d3e52d0ade3ad252e62
Alex Luketa <alex@xerini.co.uk>
2021-05-27 09:31:50 +0100

    Update build to work on windows

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -1,11 +1,22 @@
+import org.apache.tools.ant.taskdefs.condition.Os
+
 val npmInstall = task<Exec>("npmInstall") {
     workingDir = File(".")
-    commandLine = listOf("npm", "install")
+    if (Os.isFamily(Os.FAMILY_WINDOWS)) {
+        commandLine = listOf("npm.cmd", "install")
+    } else {
+        commandLine = listOf("npm", "install")
+    }
 }
 
 val buildFrontEnd = task<Exec>("buildFrontEnd") {
     workingDir = File(".")
-    commandLine = listOf("npm", "run-script", "build")
+    if (Os.isFamily(Os.FAMILY_WINDOWS)) {
+        commandLine = listOf("npm.cmd", "run-script", "build")
+    }
+    else {
+        commandLine = listOf("npm", "run-script", "build")
+    }
 }.dependsOn(npmInstall)
 
 project(":api").tasks["jar"].dependsOn(buildFrontEnd)
